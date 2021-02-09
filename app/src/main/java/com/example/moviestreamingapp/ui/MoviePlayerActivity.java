d1a619d50481772ca6fed015303aa2f140d26c8a
Sumon <sumonece15@gmail.com>
2021-02-09 17:33:27 +0600

    Movie Streaming App (1.4)

--- app/src/main/java/com/example/moviestreamingapp/ui/MoviePlayerActivity.java
+++ app/src/main/java/com/example/moviestreamingapp/ui/MoviePlayerActivity.java
@@ -2,15 +2,60 @@ package com.example.moviestreamingapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.moviestreamingapp.R;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 public class MoviePlayerActivity extends AppCompatActivity {
+
+    private PlayerView playerView;
+    private SimpleExoPlayer simpleExoPlayer;
+    public static final String VIDEO_TEST_URL = "http://clips.vorwaerts-gmbh.de/Vfe_html5.mp4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_player);
+
+        iniExoPlayer();
+
+        
+
+
+    }
+
+
+
+    private void iniExoPlayer() {
+
+        playerView = findViewById(R.id.movie_exo_player);
+        simpleExoPlayer = ExoPlayerFactory.newSimpleInstance(this);
+        playerView.setPlayer(simpleExoPlayer);
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this,
+                Util.getUserAgent(this, "appname"));
+
+        MediaSource videoSource = new ExtractorMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(Uri.parse(VIDEO_TEST_URL));
+
+        simpleExoPlayer.prepare(videoSource);
+        simpleExoPlayer.setPlayWhenReady(true);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        simpleExoPlayer.release();
     }
 }
