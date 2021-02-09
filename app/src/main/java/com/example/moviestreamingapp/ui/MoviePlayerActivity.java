2061953a8f6ed688215042b9ccc87bde8523f49e
Sumon <sumonece15@gmail.com>
2021-02-09 16:51:56 +0600

    Movie Streaming App (1.4)

---
+++ app/src/main/java/com/example/moviestreamingapp/ui/MoviePlayerActivity.java
@@ -0,0 +1,16 @@
+package com.example.moviestreamingapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.moviestreamingapp.R;
+
+public class MoviePlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_player);
+    }
+}
